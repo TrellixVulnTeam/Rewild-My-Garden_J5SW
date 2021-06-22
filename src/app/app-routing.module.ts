d17f5ef8d1d99bb9ef951b27dc0ac775775ce37b
April Bates <a.madeline.bates@gmail.com>
2021-06-22 17:22:03 +0100

    Playing with Angular Materials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+// import { HomeComponent } from '../home/home.component';
 
-// const routes: Routes = [];
+const routes: Routes = [];
 
-// @NgModule({
-//   imports: [    
-//     RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
+// const routes: Routes = [
+//   { path: 'home', component: HomeComponent},
+//   { path: '', redirectTo: '/home', pathMatch: 'full' }
+// ];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,27 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    //CommonModule and FlexLayoutModule might be better imported in a different 
+    //module (see: https://code-maze.com/get-started-angular-material/)
+    CommonModule,
+    FlexLayoutModule
   ],
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    HomeComponent
     ],
   bootstrap: [
     AppComponent
