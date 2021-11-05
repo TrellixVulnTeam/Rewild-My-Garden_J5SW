062c7e6f60463a95f714bc0b17ac95af2ed37a0f
April Bates <a.madeline.bates@gmail.com>
2021-11-05 00:45:32 +0000

    Adding padding and title

--- src/app/pages/wildlife-advice-page/wildlife-layout/wildlife-layout.component.ts
+++ src/app/pages/wildlife-advice-page/wildlife-layout/wildlife-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WildlifeAnswerSet } from '../wildlife-answers.model';
+import { WildlifeAnswers } from '../wildlife-answers.service';
 
 @Component({
   selector: 'app-wildlife-layout',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WildlifeLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(public wildlifeAnswersService: WildlifeAnswers) {
+    //here we are subscribing to the listener
+    this.wildlifeAnswersService.getAnswerUpdateListener().subscribe((retrievedAnswers: WildlifeAnswerSet[]) => {
+      document.getElementById("adviceTitle")!.innerHTML = "Plants Recommended for Your Garden";
+    });
+  }
 
   ngOnInit(): void {
   }
