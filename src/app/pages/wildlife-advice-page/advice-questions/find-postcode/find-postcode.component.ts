e70bd17891fea1c19891e32cedb73050fb32f9d7
April Bates <a.madeline.bates@gmail.com>
2021-11-25 12:29:22 +0000

    Postcode form now displays consistent err messages

--- src/app/pages/wildlife-advice-page/advice-questions/find-postcode/find-postcode.component.ts
+++ src/app/pages/wildlife-advice-page/advice-questions/find-postcode/find-postcode.component.ts
@@ -17,47 +17,37 @@ export class FindPostcodeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public postcodeFormControl: FormControl = new FormControl('', [Validators.required, this.isPostcode.bind(this)]);
-  public matcher = new MyErrorStateMatcher();
+  public postcodeFormControl: FormControl = new FormControl('', [Validators.required]);
 
   //This reads the resonse when we click enter- this is not consistent with our save button approach!!
   public onTextChange(){
+    if(this.postcodeFormControl.value == ""){
+      //If response is empty, create 'postcode required' err
+      document.getElementById('errMessage')!.innerHTML = "Postcode required.";
+      return;
+    }
     //search the ordnance survey databas to find information about the opstcode submitted
     //limiting the response to 1 so we only get exact results
     axios.get('https://api.os.uk/search/names/v1/find?maxresults=1&query=' + this.postcodeFormControl.value + '&key=' + osApiEnvironment.OS_API_KEY)
-    //this was originally ".then(function (response)" - converted due to a problem with the type of this.postcodeFormControl.value
     .then( (response) => {
         if(response.data.results[0].GAZETTEER_ENTRY.ID.toUpperCase() === this.postcodeFormControl.value.toUpperCase()){
+          document.getElementById('errMessage')!.innerHTML = "";
           console.log(JSON.stringify("hello" + response.data.results[0].GAZETTEER_ENTRY.ID, null, 2));
           console.log(JSON.stringify("hello" + response.data.results[0].GAZETTEER_ENTRY.GEOMETRY_X, null, 2));
           console.log(JSON.stringify("hello" + response.data.results[0].GAZETTEER_ENTRY.GEOMETRY_Y, null, 2));
         }
         else{
-          this.postcodeFormControl.hasError('postcode');
-          console.log('Postcode not found.');
-          //the postcode is not valid! prompt again
+          //If is in a valid postcode format but the address cannot be found, throw unfound postcode err
+          document.getElementById('errMessage')!.innerHTML = "Cannot find your postcode.";
         }
     })
-    //Try using error codes to finesse this response
+    //********** Try using error codes to finesse this response **********
     .catch(error => {
-      this.postcodeFormControl.hasError('postcode');
-      console.log('Err.');
+      //If the API throws an err because entry is not a valid postcode format, throw invalid postcode err
+      document.getElementById('errMessage')!.innerHTML = "Invalid postcode.";
     });
   }
   //displays differently if postcode automatically found
   //find hardiness, BNG and local area houses and pass that all to multichoice answers service
   //the results page will turn the location data into a radius and save info to that radius
-
-  private isPostcode(control: AbstractControl): { [key: string]: boolean } | null  {
-    //always return false
-    return ({"postcode " : false})
-  }
-}
-
-//from https://material.angular.io/components/input/examples - displays err message for incorrect form input
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
 }
