6773894155f9792c0d7775283168ac9f6715cca6
April Bates <a.madeline.bates@gmail.com>
2021-12-13 12:42:52 +0000

    wildlife-layout split further

--- src/app/pages/wildlife-advice-page/email-send/email-send.component.ts
+++ src/app/pages/wildlife-advice-page/email-send/email-send.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import axios from 'axios';
+import { Subscription } from 'rxjs';
+import { AdviceSave } from '../models/save-advice.model';
+import { UserDataSave } from '../models/save-user-data.model';
 
 @Component({
   selector: 'app-email-send',
@@ -10,62 +14,95 @@ import axios from 'axios';
 })
 export class EmailSendComponent implements OnInit {
 
-  constructor() { }
+  private extraSub: Subscription = new Subscription();
+
+  @Input() savedAdviceFinal: AdviceSave[] = [];
+  @Input() latitudeFinal: Number = 0;
+  @Input() longitudeFinal: Number = 0;
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-//   //the email that the user has submitted
-//   private email = "";
+  //the email that the user has submitted
+  private email = "";
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  matcher = new MyErrorStateMatcher();
 
-//   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-//   matcher = new MyErrorStateMatcher();
+  public processEmailData(){
+    this.email = this.emailFormControl.value;
+    console.log(this.latitudeFinal);
+    console.log(this.longitudeFinal);
+    console.log(this.savedAdviceFinal);
+    this.sendEmail()
+    this.saveUserData();
+  }
 
-//   public processEmailData(){
-//     this.email = this.emailFormControl.value;
-//     this.sendEmail()
-//     this.saveUserData();
-//   }
+  private sendEmail(){
+    if(this.savedAdviceFinal.length == 0){
+      alert("You haven't saved any advice yet! Please select some advice and then re-enter your email.");
+    }
+    else{
+      const emailContent = { email: this.email, emailBody: this.getEmailContent()};
+      axios.post('http://localhost:3000/api/sendmail', emailContent)
+      .then(response => {
+        alert("An email was sent to " + this.email + ". Happy gardening!");
+      })
+      .catch(error => {
+          console.error('There was an error!', error);
+      });
+    }
+  }
 
-//   private sendEmail(){
-//     if(this.savedAdvice.length == 0){
-//       alert("You haven't saved any advice yet! Please select some advice and then re-enter your email.");
-//     }
-//     else{
-//       const emailContent = { email: this.email, emailBody: this.getEmailContent()};
-//       axios.post('http://localhost:3000/api/sendmail', emailContent)
-//       .then(response => {
-//         alert("An email was sent to " + this.email + ". Happy gardening!");
-//       })
-//       .catch(error => {
-//           console.error('There was an error!', error);
-//       });
-//     }
-//   }
+  private getEmailContent(): String{
+    let htmlString: String = '<head><link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet"></head><body>' + 
+        '<p style="text-align: center; font-family:' + "'Playfair Display'" + ', serif; font-size: 30px;">Thank you for using Rewild My Garden!</p>' +
+        '<p style="text-align: center; font-size: 20px">Please see the advice you saved below: </p><br>';
 
-//   private getEmailContent(): String{
-//     let htmlString: String = '<head><link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet"></head><body>' + 
-//         '<p style="text-align: center; font-family:' + "'Playfair Display'" + ', serif; font-size: 30px;">Thank you for using Rewild My Garden!</p>' +
-//         '<p style="text-align: center; font-size: 20px">Please see the advice you saved below: </p><br>';
+    for(let i=0; i<this.savedAdviceFinal.length; i++){
+      htmlString = htmlString +"<div>" +
+        "<div>" +
+          '<p style="font-family:' + "'Playfair Display'" + ', serif; font-size: 20px; line-height: 0.8;">' + this.savedAdviceFinal[i].Header + '</p>' +
+          "<p><b>" + this.savedAdviceFinal[i].Justification + "</b></p>" +
+          "<p>" + this.savedAdviceFinal[i].BodyText + "</p>" +
+        '</div>' +
+        '<br>';
+    }
+    htmlString = htmlString + "</body>"
+    return htmlString;
+  }
 
-//     for(let i=0; i<this.savedAdvice.length; i++){
-//       htmlString = htmlString +"<div>" +
-//         "<div>" +
-//           '<p style="font-family:' + "'Playfair Display'" + ', serif; font-size: 20px; line-height: 0.8;">' + this.savedAdvice[i].Header + '</p>' +
-//           "<p><b>" + this.savedAdvice[i].Justification + "</b></p>" +
-//           "<p>" + this.savedAdvice[i].BodyText + "</p>" +
-//         '</div>' +
-//         '<br>';
-//     }
-//     htmlString = htmlString + "</body>"
-//     return htmlString;
-//   }
-// }
+  //Triggered by processEmailData() below
+  private saveUserData(){
+    //Correct order of coordinates in geojson is [longitude, latitude, elevation] https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.1
+    const geoJsonObj: UserDataSave = {
+      "type": "Feature",
+      "properties": {
+        "email": this.email,
+        "savedAdvice": this.savedAdviceFinal,
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          this.longitudeFinal, this.latitudeFinal
+        ]
+      }
+    };
+    //************* this line may not need to include a .subscribe()
+    //check for err????
+    this.extraSub = this.httpClient.post("http://localhost:3000/api/userData", geoJsonObj).subscribe();
+  }
 
-// /** Error when invalid control is dirty, touched, or submitted. */
-// export class MyErrorStateMatcher implements ErrorStateMatcher {
-//   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-//     const isSubmitted = form && form.submitted;
-//     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-//   }
+  ngOnDestroy() {
+    this.extraSub.unsubscribe();
+  }
+}
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
 }
--- src/app/pages/wildlife-advice-page/wildlife-layout/wildlife-layout.component.ts
+++ src/app/pages/wildlife-advice-page/wildlife-layout/wildlife-layout.component.ts
@@ -1,15 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, HostListener, OnInit } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import axios from 'axios';
 import { Subscription } from 'rxjs';
 import { AdviceGeneric } from '../models/advice.model';
 import { CompleteAnswerSet } from '../models/all-answers.model';
 import { InfoGeneric } from '../models/info.model';
 import { AdviceSave } from '../models/save-advice.model';
-import { UserDataSave } from '../models/save-user-data.model';
 import { AdviceService } from '../services/advice-boxes.service';
 import { AllAnswers } from '../services/all-answers.service';
 import { InfoService } from '../services/info-boxes.service';
@@ -287,15 +282,12 @@ export class WildlifeLayoutComponent implements OnInit {
    **********************************************************************
    ***********************************************************************/
 
-  //the email that the user has submitted
-  private email = "";
-
   //the user's longitude and latitude
   public longitude: Number = 0; 
   public latitude: Number = 0; 
 
   //array which holds saved advice 
-  private savedAdvice: AdviceSave[] = [];
+  public savedAdvice: AdviceSave[] = [];
 
   /* An array of saved advice is created and passed to all children. When 'save this advice' is clicked in an advice box, it adds a data entry to this
   array. When a user submits their email, this array is wrapped up with the email and location (retrieved from CompleteAnswerSet) and added to the database as geojson.
@@ -312,83 +304,5 @@ export class WildlifeLayoutComponent implements OnInit {
         this.savedAdvice.splice(index,1);
       }
     });  
-  }
-
-  //Triggered by processEmailData() below
-  private saveUserData(){
-    //Correct order of coordinates in geojson is [longitude, latitude, elevation] https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.1
-    const geoJsonObj: UserDataSave = {
-      "type": "Feature",
-      "properties": {
-        "email": this.email,
-        "savedAdvice": this.savedAdvice,
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          this.longitude, this.latitude
-        ]
-      }
-    };
-    //************* this line may not need to include a .subscribe()
-    //check for err????
-    this.extraSub = this.httpClient.post("http://localhost:3000/api/userData", geoJsonObj).subscribe();
-  }
-
-  /***********************************************************************
-   ***********************************************************************
-   ************************ LOGIC TO HANDLE EMAILS ***********************
-   ***********************************************************************
-   ***********************************************************************/
-
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  matcher = new MyErrorStateMatcher();
-
-  public processEmailData(){
-    this.email = this.emailFormControl.value;
-    this.sendEmail()
-    this.saveUserData();
-  }
-
-  private sendEmail(){
-    if(this.savedAdvice.length == 0){
-      alert("You haven't saved any advice yet! Please select some advice and then re-enter your email.");
-    }
-    else{
-      const emailContent = { email: this.email, emailBody: this.getEmailContent()};
-      axios.post('http://localhost:3000/api/sendmail', emailContent)
-      .then(response => {
-        alert("An email was sent to " + this.email + ". Happy gardening!");
-      })
-      .catch(error => {
-          console.error('There was an error!', error);
-      });
-    }
-  }
-
-  private getEmailContent(): String{
-    let htmlString: String = '<head><link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet"></head><body>' + 
-        '<p style="text-align: center; font-family:' + "'Playfair Display'" + ', serif; font-size: 30px;">Thank you for using Rewild My Garden!</p>' +
-        '<p style="text-align: center; font-size: 20px">Please see the advice you saved below: </p><br>';
-
-    for(let i=0; i<this.savedAdvice.length; i++){
-      htmlString = htmlString +"<div>" +
-        "<div>" +
-          '<p style="font-family:' + "'Playfair Display'" + ', serif; font-size: 20px; line-height: 0.8;">' + this.savedAdvice[i].Header + '</p>' +
-          "<p><b>" + this.savedAdvice[i].Justification + "</b></p>" +
-          "<p>" + this.savedAdvice[i].BodyText + "</p>" +
-        '</div>' +
-        '<br>';
-    }
-    htmlString = htmlString + "</body>"
-    return htmlString;
-  }
-}
-
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
 }
